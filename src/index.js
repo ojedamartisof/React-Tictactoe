a35f7d23732ba036916587a507aa315e52fb65a9
ojedamartisof <ojedamartinasofia@gmail.com>
2020-11-03 17:20:48 -0300

    Adding files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+//React
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+//Javascript
+import 'jquery';
+import 'bootstrap';
+import 'popper.js';
+
+//Components
+import Home from './components/home';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
